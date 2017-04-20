a13c8f0cecff8e214966e6c9c16f5c8d13bbad73
Nicole Francisco <Nicole Francisco>
2017-04-20 13:11:17 -0400

    changed audio play and animations

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
     $('#fullpage').fullpage({
 		anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
@@ -9,10 +10,10 @@ $(document).ready(function() {
 	var renderer = PIXI.autoDetectRenderer(1500,100, {transparent: true});
 	$('#addPixi').append(renderer.view);
 	var stage = new PIXI.Container();
-	var coach = PIXI.Sprite.fromImage('../images/coach.png');
-	coach.anchor.x = 0.5;
+	var coach = PIXI.Sprite.fromImage('images/coach.png');
+	coach.anchor.x = 0.1;
 	coach.anchor.y = 0.5;
-	coach.position.x = 10;
+	coach.position.x = 1;
 	coach.position.y = 50;
 	coach.width = 200;
 	coach.height = 100;
@@ -28,14 +29,25 @@ $(document).ready(function() {
 		}
 		renderer.render(stage);
 	}
-	
+
+$('#picss > div:gt(0)').hide();
+
+setInterval(function() {
+	$('#picss > div:first')
+	.fadeOut(1000)
+	.next()
+	.fadeIn(1000)
+	.end()
+	.appendTo('#picss');
+}, 5000);
 });
+
 
 function atv() {
 	var renderer1 = PIXI.autoDetectRenderer(1500,100, {transparent: true});
 		$('#addPixi1').append(renderer1.view);
 		var stage1 = new PIXI.Container();
-		var atv = PIXI.Sprite.fromImage('../images/red.png');
+		var atv = PIXI.Sprite.fromImage('images/red.png');
 		atv.anchor.x = 0.5;
 		atv.anchor.y = 0.5;
 		atv.position.x = 10;
@@ -51,6 +63,8 @@ function atv() {
 			requestAnimationFrame(render);
 			if(atv.position.x < 2000) {
 				atv.position.x +=10;
+				var aud = document.getElementById('beepThree');
+			aud.play();
 			}
 			if (atv.position.x >= 1500) {
 				$('#muddys').show();
@@ -58,17 +72,21 @@ function atv() {
 				$('#muddys').removeClass('animated zoomIn');
 				$('#muddys').addClass('animated slideOutDown');
 				$('#muddys').css('-animation-duration', '3s');
-				$('#slide1').css('background-image', 'url("../images/hogwaller.png")');
+				$('#slide1').css('background-image', 'url("images/hogwaller.png")');
 				$('#slide1').css('background-position', 'center');
 				$('#slide1').css('background-size', '50%');
 				$('#slide1').css('background-repeat', 'no-repeat');
+				
 			}
 			
 			renderer1.render(stage1);
 			
 			$('#what').remove();
+			
 		}
 }
+
+
 
 function shake() {
 	$('#myth1').addClass('shake-slow shake-constant');
@@ -99,15 +117,19 @@ function StopBoar(soundobje) {
 	beepTwo.pause();
 }
 	
-function PlayAtv(soundobjec) {
-	var beepThree = document.getElementById(soundobjec);
-		beepThree.currentTime = 24;
-		beepThree.play();
-}
+
 	
 function StopAtv(soundobjec) {
 	var beepThree = document.getElementById(soundobjec);
 	beepThree.pause();
 }	
 
-
+function cycleImages() {
+	var $active = $('#cycler .active');
+	var $next = ($active.next().length > 0) ? $active.next() : $('#cycler img:first');
+	$next.css('z-index',2);
+	$active.fadeOut(1500, function() {
+		$active.css('z-index',1).show().removeClass('active');
+		$next.css('z-index',3).addClass('active');
+	});
+}
